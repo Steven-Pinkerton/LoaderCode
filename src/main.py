c20041722876fc94aa505b3c276784b1d7c26168
Steven <steven.a.pinkerton@gmail.com.com>
2023-08-13 18:23:45 +0100

    Implementation complete at the moment

--- src/main.py
+++ src/main.py
@@ -1,112 +1,144 @@
-import fbx
 import json
-
+import os
+import fbx
 
 def fbx_to_json(fbx_file_path):
-    
-    fbx_file = fbx.load(fbx_file_path)
-    
-    output = {}
-    
-    # Hierarchy
-    output['hierarchy'] = [] 
-    for c in fbx_file.hierarchy:
-        hierarchy_data = {
-            'name': c.name,
-            'parent': c.parent.name if c.parent else None,
-            'children': [ch.name for ch in c.children]
-        }
-        output['hierarchy'].append(hierarchy_data)
 
-    # Nodes 
-    output['nodes'] = []
-    for node in fbx_file.nodes:
-        node_data = {
-            'name': node.name,
-            'transformation': node.transformation.tolist(),
-            'hierarchy': node.hierarchy.name
-        }
-        output['nodes'].append(node_data)
+    # Load FBX 
+    manager = fbx.FbxManager()
+    importer = fbx.FbxImporter(manager)
+    scene = importer.importFile(fbx_file_path)
+
+    output = {
+        'metadata': {},
+        'nodes': [],
+        'meshes': [],
+        'materials': [],
+        'animations': [],
+        'cameras': [],
+        'lights': []
+    }
+
+    # Metadata
+    output['metadata']['creator'] = scene.GetSceneInfo().original_author.decode()
+
+    # Nodes
+    for node in scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeCount():
+        attr = scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeByIndex(i)
+        attr_type = attr.GetAttributeType()
+        if attr_type == fbx.FbxNodeAttribute.eMesh:
+            # Extract mesh node data
+            for i in range(scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeCount()): 
+                attr = scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeByIndex(i)
+                if attr.GetAttributeType() == fbx.FbxNodeAttribute.eMesh:
+                    mesh = attr.GetNode() 
+                    mesh_data = {
+                        'name': mesh.GetName(),
+                        'vertices': mesh.GetControlPoints(), 
+                        'polygons': mesh.GetPolygonVertices()
+                    }
+                    output['meshes'].append(mesh_data)
+            
+            
+        elif attr_type == fbx.FbxNodeAttribute.eCamera:
+            # Extract camera node data
+            for i in range(scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeCount()):
+                attr = scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeByIndex(i) 
+                if attr.GetAttributeType() == fbx.FbxNodeAttribute.eCamera:
+                    cam = attr.GetNode()
+                    camera_data = {
+                        'name': cam.GetName(),
+                        'position': list(cam.LclTranslation.Get()),
+                        'rotation': list(cam.LclRotation.Get()), 
+                        'fov': cam.FieldOfView.Get()
+                    }
+                    output['cameras'].append(camera_data)
+
         
-    # Extract nodes
-    output['nodes'] = []
-    for node in fbx_file.nodes:
-        node_data = {
-            'name': node.name,
-            'transformation': node.transformation.tolist() 
-        }
-        output['nodes'].append(node_data)
+        elif attr_type == fbx.FbxNodeAttribute.eLight:
+            # Extract light node data
+            for i in range(scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeCount()):
+                attr = scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeByIndex(i)
+                if attr.GetAttributeType() == fbx.FbxNodeAttribute.eLight:
+                    light = attr.GetNode()
+                    light_data = {
+                        'name': light.GetName(),
+                        'type': light.LightType.Get(),
+                        'position': list(light.LclTranslation.Get()),
+                        'rotation': list(light.LclRotation.Get())
+                    }
+                    output['lights'].append(light_data)
+        else:
+            for i in range(scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeCount()):
+                attr = scene.GetSrcObject(fbx.FbxNode.ClassId).GetNodeAttributeByIndex(i)
+                if attr.GetAttributeType() == fbx.FbxNodeAttribute.eNull:
+                    node = attr.GetNode()
+                    node_data = {
+                        'name': node.GetName(),
+                        'translation': list(node.LclTranslation.Get()),
+                        'rotation': list(node.LclRotation.Get()),
+                        'scaling': list(node.LclScaling.Get()) 
+                    }
+                    output['nodes'].append(node_data)
 
-    # Extract meshes
-    output['meshes'] = [] 
-    for mesh in fbx_file.meshes:
+    # Meshes
+    for mesh in scene.GetSrcObject(fbx.FbxMesh.ClassId):
         mesh_data = {
-            'name': mesh.name,
-            'vertices': mesh.vertices.tolist(),
-            'faces': mesh.faces.tolist()
+            'name': mesh.GetName(),
+            'vertices': mesh.GetControlPoints(),
+            'polygons': mesh.GetPolygonVertices()
         }
         output['meshes'].append(mesh_data)
-        
-    # Extract materials
-    output['materials'] = []
-    for material in fbx_file.materials:
-        material_data = {
-            'name': material.name,
-            'shading_model': material.shading_model, 
-            'properties': material.properties
-        }
-        output['materials'].append(material_data)
 
-    # Textures
-    output['textures'] = []
-    for texture in fbx_file.textures:
-        texture_data = {
-            'name': texture.name,
-            'filename': texture.filename,
-            'properties': texture.properties
+    # Materials 
+    for mat in scene.GetSrcObject(fbx.FbxSurfaceMaterial.ClassId):
+        mat_data = {
+            'name': mat.GetName(),
+            'shading_model': mat.ShadingModel.Get(),
+            'properties': mat.GetProperties()
         }
-        output['textures'].append(texture_data)
+        output['materials'].append(mat_data) 
 
     # Animations
-    output['animations'] = []
-    for anim in fbx_file.animations:
-        animation_data = {
-            'name': anim.name,
-            'start': anim.start,
-            'end': anim.end,
-            'curves': [{'node': c.node.name, 'curves': c.curves} 
-                    for c in anim.curves]  
+    for anim in scene.GetSrcObject(fbx.FbxAnimStack.ClassId):
+        anim_data = {
+            'name': anim.GetName(),
+            'start': anim.LocalStart.Get(),
+            'end': anim.LocalStop.Get()
         }
-        output['animations'].append(animation_data)
-        
-    # Cameras 
-    output['cameras'] = []    
-    for cam in fbx_file.cameras:
-        camera_data = {
-            'name': cam.name,
-            'position': cam.position.tolist(),
-            'interest': cam.interest.tolist(),
-            # other properties
-        }
-        output['cameras'].append(camera_data)   
+        output['animations'].append(anim_data)
 
+    # Cameras
+    for cam in scene.GetSrcObject(fbx.FbxCamera.ClassId):
+        cam_data = {
+            'name': cam.GetName(),
+            'position': list(cam.Position.Get()),
+            'up_vector': list(cam.UpVector.Get()),
+            'interest_position': list(cam.InterestPosition.Get()),
+        }
+        output['cameras'].append(cam_data)
+    
     # Lights
-    output['lights'] = []
-    for light in fbx_file.lights:
+    for light in scene.GetSrcObject(fbx.FbxLight.ClassId):
         light_data = {
-            'name': light.name,
-            'type': light.light_type,
-            'position': light.position.tolist(),
-            # other properties
+            'name': light.GetName(),
+            'type': light.LightType.Get(),
+            'position': list(light.Position.Get()),
+            'color': list(light.Color.Get())
         }
         output['lights'].append(light_data)
-        
-    with open('scene.json', 'w') as f:
-        json.dump(output, f)
-        
-        with open(fbx_file_path.replace('.fbx', '.json'), 'w') as f:
-            json.dump(output, f)
 
-if __name__ == "__main__":
-    fbx_file_path = "LoaderCoder/Lunar_Wolf.fbx" 
-    fbx_to_json(fbx_file_path)
+    # Write JSON
+    with open('output.json', 'w') as f:
+        json.dump(output, f)
+
+def main():
+    # Get project root directory path
+    root_dir = os.path.dirname(os.path.abspath(__file__)) 
+    fbx_path = os.path.join(root_dir, '../Lunar_wolf.fbx')
+
+
+    fbx_to_json(fbx_path)
+
+if __name__ == '__main__':
+    main()
